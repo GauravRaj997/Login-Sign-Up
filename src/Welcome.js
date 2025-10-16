786fe143dc2676e97297f0ffefbea7c5da6c0ae5
GauravRaj997 <crk0732@gmail.com>
2025-10-16 10:02:38 +0530

    Login-Sign Up

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Welcome.css'; 
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default Welcome;
